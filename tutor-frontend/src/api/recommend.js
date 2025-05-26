5f579d2af018cdb45d613b922212c3ee785bcc0a
Guess-who-i-m <1642166930xkb@gmail.com>
2025-05-26 20:47:52 +0800

    Revert "update 完成应聘列表，具有分页功能"
    This reverts commit d1cdac8c4ff6207f462c39f750726615122f9647.

--- tutor-frontend/src/api/recommend.js
+++ tutor-frontend/src/api/recommend.js
@@ -4,9 +4,3 @@ import request from '@/utils/request.js'
 export const recommendAddService = (recommendData) => {
     return request.post("/recommend/publishRecommend", recommendData)
 }
-
-// 文章列表查询
-// queryString的调用方式
-export const recommendListService = (params) =>{
-    return request.get('/recommend/getMyRecommends', {params:params})
-}
