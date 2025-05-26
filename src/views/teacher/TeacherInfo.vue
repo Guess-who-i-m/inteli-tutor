df4c59972728a468a980ef5badfd410a2548c406
zwh11wzy <estridyzw11@gmail.com>
2025-05-26 14:54:03 +0800

    前端信息页优化

--- src/views/teacher/TeacherInfo.vue
+++ src/views/teacher/TeacherInfo.vue
@@ -1,3 +1,5 @@
+<!-- <script lang="ts" setup>
+import { reactive } from 'vue' -->
 <script setup>
 import { ref } from 'vue'
 const userInfo = ref({
@@ -28,7 +30,9 @@ import useUserInfoStore from '@/stores/userInfo'
 
 const userInfoStore = useUserInfoStore()
 
-const getUserInfo = ()=>{
+const getUserInfo = () => {
+    // 直接赋值对象属性，不需要 .value
+    // Object.assign(userInfo, userInfoStore.info)
     userInfo.value = userInfoStore.info
 }
 
@@ -38,13 +42,13 @@ import { userInfoUpdateService } from '@/api/user'
 import { ElMessage } from 'element-plus'
 import { teacherInfoUpdateService } from '@/api/teacher'
 
-const modifyUserInfo = async()=>{
+const modifyUserInfo = async () => {
     // 虽然这个vue文件里只控制了username、gender、grade、addr和phoneNum等信息
     // 但由于pinia里存储了avatar，所以发送请求包含了原本avatar的数据，不用担心avatar丢失
     let result = await teacherInfoUpdateService(userInfo.value)
 
-    ElMessage.success(result.message ? result.message: "修改成功")
-    
+    ElMessage.success(result.message ? result.message : "修改成功")
+
     // 修改pinia中的信息
     userInfoStore.setInfo(userInfo.value)
 }
@@ -67,20 +71,45 @@ const modifyUserInfo = async()=>{
                         <el-input v-model="userInfo.username" disabled></el-input>
                     </el-form-item>
 
-                    <el-form-item label="教师性别" prop="gender">
+                    <!-- <el-form-item label="教师性别" prop="gender">
                         <el-input v-model="userInfo.gender"></el-input>
+                    </el-form-item> -->
+
+                    <el-form-item label="教师性别" prop="gender">
+                        <el-radio-group v-model="userInfo.gender">
+                            <el-radio value="M">男生</el-radio>
+                            <el-radio value="F">女生</el-radio>
+                        </el-radio-group>
                     </el-form-item>
 
                     <el-form-item label="就读学校" prop="school">
                         <el-input v-model="userInfo.school"></el-input>
                     </el-form-item>
 
-                    <el-form-item label="学校层次" prop="schLevel">
+                    <!-- <el-form-item label="学校层次" prop="schLevel">
                         <el-input v-model="userInfo.schLevel"></el-input>
+                    </el-form-item> -->
+
+                    <el-form-item label="学校层次" label-position="eb" prop="schLevel">
+                        <el-radio-group v-model="userInfo.schLevel" aria-label="label position">
+                            <el-radio-button value="eb">二本</el-radio-button>
+                            <el-radio-button value="yb">一本</el-radio-button>
+                            <el-radio-button value="eyy">211</el-radio-button>
+                            <el-radio-button value="jbw">985</el-radio-button>
+                            <el-radio-button value="syl">双一流</el-radio-button>
+                        </el-radio-group>
                     </el-form-item>
 
-                    <el-form-item label="学历层次" prop="eduBg">
+                    <!-- <el-form-item label="学历层次" prop="eduBg">
                         <el-input v-model="userInfo.eduBg"></el-input>
+                    </el-form-item> -->
+
+                    <el-form-item label="学历层次" label-position="master" prop="eduBg">
+                        <el-radio-group v-model="userInfo.eduBg" aria-label="label position">
+                            <el-radio-button value="bachelor">本科</el-radio-button>
+                            <el-radio-button value="master">硕士</el-radio-button>
+                            <el-radio-button value="doctor">博士</el-radio-button>
+                        </el-radio-group>
                     </el-form-item>
 
                     <el-form-item label="联系方式" prop="phoneNum">
